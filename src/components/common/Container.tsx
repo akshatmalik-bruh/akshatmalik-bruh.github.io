4d8214e8d7fad8ccc65c9ee9a09451285bf9dc78
Akshat malik <akshatf8lmalik@gmail.com>
2026-08-29 01:16:25 +0530

    Add optional id prop to Container component

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 export default function Container({
   children,
   className,
+  id,
   ...props
 }: {
   children: React.ReactNode;
   className?: string;
+  id?: string;
 }) {
   return (
     <div
+      id={id}
       className={`animate-fade-in-blur container mx-auto max-w-3xl px-4 ${className}`}
       {...props}
     >
